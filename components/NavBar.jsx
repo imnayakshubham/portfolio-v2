4f1492945c87e1ab36b7968b00fd21b991baee70
imnayakshubham <imnayakshubham@gmail.com>
2023-01-29 16:26:59 +0530

    updated navbar heading

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,21 +1,39 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import DarkModeToggler from './DarkModeToggler/DarkModeToggler';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { ImCross } from 'react-icons/im';
+import { Typewriter } from 'react-simple-typewriter';
 
 const NavBar = () => {
   const [isMobileView, setIsMobileView] = useState(false);
-  return (
+  const [cursorStyle, setcursorStyle] = useState("🖋️")
 
+  const handleDone = (value) => {
+    setcursorStyle(" ")
+    return
+  }
+
+  return (
     <nav className={`w-full fixed top-0 left-0 right-0 z-10000 ${isMobileView ? "bg-[#121212] text-white" : ""}`}>
       <div className="justify-between mx-auto lg:max-w-8xl md:items-center md:flex md:px-4 backdrop-blur-sm">
         <div>
           <div className="flex items-center justify-between p-3 md:p-5 md:block">
             {/* LOGO */}
             <Link href="/">
-              <h2 className="text-2xl font-bold">Nayak Shubham</h2>
+              <h2 className="text-2xl font-bold">
+                <Typewriter
+                  words={['Hi there 👋', 'Hello 👋', 'Nayak Shubham']}
+                  loop={1}
+                  cursor
+                  cursorStyle={cursorStyle}
+                  typeSpeed={70}
+                  deleteSpeed={50}
+                  delaySpeed={1000}
+                  onLoopDone={handleDone}
+                />
+              </h2>
             </Link>
             {/* HAMBURGER BUTTON FOR MOBILE */}
             <div className="md:hidden flex gap-3 items-center">
