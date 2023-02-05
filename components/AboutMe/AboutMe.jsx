7952d4d4540827e378c17fea01f54c617e205c8a
imnayakshubham <imnayakshubham@gmail.com>
2023-02-05 12:54:25 +0530

    layout fix in about me

--- components/AboutMe/AboutMe.jsx
+++ components/AboutMe/AboutMe.jsx
@@ -4,11 +4,11 @@ import { Socials } from '../Socials/Socials'
 
 const AboutMe = () => {
     return (
-        <section name="about" className='flex justify-center align-middle flex-col h-screen aboutme'>
+        <section name="about" className='flex justify-center align-middle flex-col h-screen gap-2 aboutme'>
             <h1 className="text-6xl font-bold text-center">
                 {"Nayak Shubham"}
             </h1>
-            <p className="text-600 my-4 text-center">
+            <p className="text-600 text-center">
                 Experienced {' '}
                 <span style={{ color: '#673ab7', fontWeight: 'bold' }}>
                     <Typewriter
@@ -20,19 +20,17 @@ const AboutMe = () => {
                         typeSpeed={70}
                         deleteSpeed={50}
                         delaySpeed={1000}
-                    // onLoopDone={handleDone}
-                    // onType={handleType}
                     />
                     {" "}
                 </span>
             </p>
-            <Socials />
-            <div className="flex justify-center gap-4 flex-wrap my-4  items-center">
+            <div className="flex justify-center gap-2 flex-wrap items-center">
                 <i className="fa-solid fa-location-dot text-2xl"></i>
                 <p className="text-600 text-center">
                     Ahmedabad, Gujarat, India
                 </p>
             </div>
+            <Socials />
         </section>
     )
 }
